43435b8835e32005954fc515a36c8a5fdb976fa4
Todd <toddlenhart@gmail.com>
2014-11-26 11:30:23 -0700

    Testing morris.js.

--- Dashboard/static/js/dashboard.js
+++ Dashboard/static/js/dashboard.js
@@ -1,2 +1,15 @@
 // Base javascript file.
 // Testing minification.
+
+new Morris.Line({
+    element: 'testgraph',
+    data: [
+        {year: '2008', value: '200'},
+        {year: '2009', value: '200'},
+        {year: '2010', value: '175'},
+        {year: '2011', value: '250'}
+    ],
+    xkey: 'year',
+    ykeys: ['value'],
+    labels: ['Incidents']
+});
