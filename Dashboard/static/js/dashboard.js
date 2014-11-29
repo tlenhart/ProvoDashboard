64e26c9f4ea5f690799afbddb58b86cc79d8c699
Todd <toddlenhart@gmail.com>
2014-11-29 14:11:33 -0700

    Added a second graph and testing with Bootstrap. The graphs are overlapping right now, not completely sure why.

--- Dashboard/static/js/dashboard.js
+++ Dashboard/static/js/dashboard.js
@@ -13,3 +13,20 @@ new Morris.Line({
     ykeys: ['value'],
     labels: ['Incidents']
 });
+
+
+new Morris.Line({
+    element: 'testgraph2',
+    data: [
+        {year: '2008', value: '200'},
+        {year: '2009', value: '200'},
+        {year: '2010', value: '175'},
+        {year: '2011', value: '250'}
+    ],
+    xkey: 'year',
+    ykeys: ['value'],
+    labels: ['Incidents']
+});
+
+//$('#testgraph').addClass("col-md-6");
+//$('#testgraph2').addClass("col-md-4");
