5ef0e0f7bf68db1a86f096d1b7f86cecadce0fbc
Todd <toddlenhart@gmail.com>
2014-12-01 12:35:51 -0700

    Think I fixed the django admin thing.

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -4,6 +4,13 @@ from django.db import models
 #   Not sure if I have set this up properly (the models)
 #   Might (or will) need to change so that the databases are set up properly.
 #   The fields in each class are columns in the database. I think I have this wrong.
+#
+#   Anything that is a DecimalField requires a max_digits and a decimal_places.
+#   These may need to be adjusted based on how we want the numbers to be saved/stored.
+#
+#   No keys currently exist in the tables that point to other tables...
+#   For example, community = models.ForeignKey(Community)
+#
 ##
 
 # Create your models here.
@@ -30,7 +37,7 @@ class RecreationCultural(models.Model):
 class Economic(models.Model):
     active_business_licenses = models.IntegerField('Active number of business licenses.',default=0)
     building_permits_given = models.IntegerField('Number of building permits distributed.',default=0)
-    unemployment_rate = models.DecimalField('Unemployment rate as a percentage.',default=0.0) # Need to decide how this is going to be stored. 0.33 or 33 for 33%.
+    unemployment_rate = models.DecimalField('Unemployment rate as a percentage.',default=0.0,max_digits=5,decimal_places=5) # Need to decide how this is going to be stored. 0.33 or 33 for 33%.
     houses_sold = models.IntegerField('Number of houses sold.',default=0)
 
     def __str__(self):
@@ -39,8 +46,8 @@ class Economic(models.Model):
 class Environment(models.Model):
     waste_collected = models.IntegerField('Number of truckloads of waste collected.',default=0) # Number of Truckloads
     recyclables_collected = models.IntegerField('Number of truckloads of recyclables collected.',default=0) # Number of Truckloads
-    level_reserve_water_tanks = models.DecimalField('Level of reserve water tanks as a percentage of capacity.',default=0.0) # % of capacity. Need to decide how this is going to be stored. 0.33 or 33 for 33%.
-    gallons_water_treated = models.DecimalField('Gallons of water treated.',default=0.0) # Could maybe change to an integer.
+    level_reserve_water_tanks = models.DecimalField('Level of reserve water tanks as a percentage of capacity.',default=0.0,max_digits=5,decimal_places=5) # % of capacity. Need to decide how this is going to be stored. 0.33 or 33 for 33%.
+    gallons_water_treated = models.DecimalField('Gallons of water treated.',default=0.0,max_digits=14,decimal_places=2) # Could maybe change to an integer. max_digits=14 and decimal_places=2 allows up to 999,999,999,999.99 gallons of water to be stored. (Unless I'm mistaken...)
     home_inspections = models.IntegerField('Number of home inspections.',default=0)
     business_inspections = models.IntegerField('Number of business inspections.',default=0)
 
@@ -50,18 +57,18 @@ class Environment(models.Model):
 class Safety(models.Model):
     nonviolent_crimes_reported = models.IntegerField('Number of nonviolent crimes reported.',default=0)
     violent_crimes_reported = models.IntegerField('Number of violent crimes reported.',default=0)
-    police_response_time = models.DecimalField('Police response time (high priority) in ...',default=0.0) # Might need to change to police_response_time_high_priority
+    police_response_time = models.DecimalField('Police response time (high priority) in ...',default=0.0,max_digits=10,decimal_places=2) # Might need to change to police_response_time_high_priority
     fires_responded = models.IntegerField('Number of fires responded to.',default=0)
-    fire_response_time = models.DecimalField('Fire department response time in ...',default=0.0)
+    fire_response_time = models.DecimalField('Fire department response time in ...',default=0.0,max_digits=10,decimal_places=2)
 
     def __str__(self):
         return "Database table for Public Safety."
 
 class Transportation(models.Model):
     transportation_injuries = models.IntegerField('Transportation injuries.',default=0)
-    ontime_percent = models.DecimalField('Percentage of services on time.',default=0.0)
+    ontime_percent = models.DecimalField('Percentage of services on time.',default=0.0,max_digits=5,decimal_places=5)
     auto_accidents = models.IntegerField('Number of auto accidents.',default=0)
-    public_ridership = models.DecimalField('Ridership of public transportation in thousands.',default=0.0) # In thousands. Multiply by 1000 if necessary. Storing 19.1 would equal 19,100.
+    public_ridership = models.DecimalField('Ridership of public transportation in thousands.',default=0.0,max_digits=7,decimal_places=3) # In thousands. Multiply by 1000 if necessary. Storing 19.1 would equal 19,100.
     potholes_filed_YTD = models.IntegerField('Potholes filled YTD.',default=0)
 
     def __str__(self):
--- ProvoDashboard/urls.py
+++ ProvoDashboard/urls.py
@@ -1,12 +1,29 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from Dashboard import *
+from django.contrib import admin
+
+# If the admin interface isn't working for you, delete the database, run manage.py syncdb, and set up the superuser.
+# Then run:
+#   manage.py makemigrations
+#   manage.py migrate
+# The last import statement in this file should be: from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ProvoDashboard.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Will need more urls for detailed graph pages. These need to be generated...
+
     url(r'^$', 'Dashboard.views.index', name='index'),
-    # url(r'^admin/$', include(admin.site.urls)), # Doesn't seem to be working right now. Don't know what broke it...
+    url(r'^community', 'Dashboard.views.community', name='community'),
+    url(r'^economic', 'Dashboard.views.economic', name='economic'),
+    url(r'^environment', 'Dashboard.views.environment', name='environment'),
+    url(r'^recreation', 'Dashboard.views.recreation', name='recreation'),
+    url(r'^cultural', 'Dashboard.views.cultural', name='cultural'), # cultural and recreation go to the same page.
+    url(r'^safety', 'Dashboard.views.safety', name='safety'),
+    url(r'^transportation', 'Dashboard.views.transportation', name='transportation'),
+    url(r'^admin/', include(admin.site.urls)),
 )
