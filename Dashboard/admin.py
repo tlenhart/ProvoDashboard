da8f63c68b75d9c1b59cf2e9328b50b3ab63ecbb
Colton <coltonparry@gmail.com>
2014-12-04 22:18:12 -0700

    registered models with admin

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import SampleData, Community, RecreationCultural, Economic, Environment, Safety, Transportation
+
+
+admin.site.register(SampleData)
+admin.site.register(Community)
+admin.site.register(RecreationCultural)
+admin.site.register(Economic)
+admin.site.register(Environment)
+admin.site.register(Safety)
+admin.site.register(Transportation)
