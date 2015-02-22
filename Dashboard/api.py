f8d9a4b93612b05aa39c9d5a0cbbdb8d83435bc7
Colton <coltonparry@gmail.com>
2015-02-22 12:12:58 -0700

    Changed Models, Changed Database Layout and adjust URLS / ADMIN /default.JSON

--- Dashboard/api.py
+++ Dashboard/api.py
@@ -1,14 +1,15 @@
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-from Dashboard.models import Community, RecreationCultural, Economic, Safety  # ,Civic, Government
+from Dashboard.models import Safety  # ,Civic, Government
 
 # See https://django-tastypie.readthedocs.org/en/latest/interacting.html for info about interacting with the api.
+
 
 class SafetyResource(ModelResource):
     class Meta:
         queryset = Safety.objects.all()
         resource_name = 'safety'
 
-
+"""
 class EconomicResource(ModelResource):
     class Meta:
         queryset = Economic.objects.all()
@@ -19,10 +20,10 @@ class EconomicResource(ModelResource):
             'year': ALL,
         }
 
-        """
+
             Call /api/economic/?year=2014 to filter objects by the year 2014.
             To make a column filterable, it must be added to the filtering object above.
-        """
+
 
 
 class CivicResource(ModelResource):
@@ -35,5 +36,5 @@ class GovernmentResource(ModelResource):
     class Meta:
         queryset = RecreationCultural.objects.all()  # Needs to change to Government when the models are updated.
         resource_name = 'government'
-
+"""
 
--- Dashboard/models.py
+++ Dashboard/models.py
@@ -36,165 +36,64 @@ MONTHS = (
     ("December", "December")
 )
 
-DEPARTMENT = (
-    ("Community", "Community Neighborhood Livability"),
-    ("Culture", "Cultural and Recreation"),
-    ("Economic", "Economic Health"),
-    ("Environment", "Environmental Health"),
-    ("Safety", "Community Safety"),
-    ("Transportation", "Transportation")
-)
 
-COMMUNITY_CATEGORIES = (
-    ("Pets Rescued", "Pets Rescued at Shelter"),
-    ("911 Calls", "911 Calls"),
-    ("911 Response", "911 Phone Response Time (seconds)"),
-    ("Noise Complaints", "Noise Complaints"),
-    ("Code Violations", "Code Violations")
-
-)
-
-RECREATION_CATEGORIES = (
-    ("Major Attractions", "Major Attractions Attended (# of attendees"),
-    ("Community Involvement", "Community Program Involvement (# of patrons"),
-    ("Rec Center Patrons", "Recreation Center Patrons"),
-    ("Golf Course Patrons", "Golf Course Patrons"),
-    ("Ice Arena Participants", "Ice Arena Participants")
-)
-
-ECONOMIC_CATEGORIES = (
-    ("Active Licenses", "Active Number of Business Licenses"),
-    ("Building Permits", "Building Permits Issued"),
-    ("Unemployment Rate", "Unemployment Rate"),
-    ("Houses Sold", "Houses Sold")
-)
-
-ENVIRONMENT_CATEGORIES = (
-    ("Waste Collected", "Waste Collected (Truck Loads)"),
-    ("Recyclables Collected", "Recyclables Collected (Truck Loads"),
-    ("Water Level", "Level of Reserve Water Tanks (% of capacity)"),
-    ("Water Treated", "Gallons of Water Treated"),
-    ("Home Inspections", "Home Inspections"),
-    ("Business Inspections", "Business Inspections")
-)
-
-SAFETY_CATEGORIES = (
-    ("Nonviolent Crimes", "Nonviolent Crimes Reported"),
-    ("Violent Crimes", "Violent Crimes Reported"),
-    ("Police Response Time", "Police Response Time (High Priority)"),
-    ("Fires Responded", "Fires Responded"),
-    ("Fire Response Time", "Fire Response Time")
-)
-
-TRANSPORTATION_CATEGORIES = (
-    ("Transportation Injuries", "Transportation Injuries"),
-    ("On time Rate", "Percentage On Time"),
-    ("Auto Accidents", "Auto Accidents"),
-    ("Ridership", "Public Ridership (In thousands)"),
-    ("Pot holes Filled", "Pot Holes Filled (YTD)")
-)
-
-
-class Community(models.Model):
-    # pets_rescued_at_shelter = models.IntegerField('Pets rescued at shelters', default=0)
-    # calls_911 = models.IntegerField('Number of 911 calls', default=0)
-    # might want to change to models.TimeField #
-    # response_time_911_calls = models.IntegerField('Response time for 911 calls in seconds.', default=0)
-    # noise_complaints = models.IntegerField('Number of noise complaints in a month.', default=0)
-    # code_violations = models.IntegerField('Number of code violations in a month.', default=0)
+class SubCategories(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=COMMUNITY_CATEGORIES, max_length=100)
+    sub_category = models.CharField(max_length=300, default="Please Add A Category")
+    description = models.CharField(max_length=1000, null=False)
+    main_category = models.CharField(max_length=300, null=True)
+
+    def __str__(self):
+        return "{0} **{1}** ".format(self.sub_category, self.main_category)
+
+
+class PublicSafety(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
+    category = models.ForeignKey(SubCategories)
     value = models.DecimalField(decimal_places=2, max_digits=11)
     month = models.CharField(choices=MONTHS, max_length=20)
     year = models.IntegerField(choices=YEARS)
 
     def __str__(self):
-        return "Database table for Community, Neighborhood, and Livability."
+        return "{0} - {1}, {2}".format(self.category, self.month, self.year)
 
 
-class RecreationCultural(models.Model):
-    # Number of attendees
-    # major_attractions_attended = models.IntegerField('Number of attendees at major attractions.',default=0)
-    # Number of patrons
-    # community_program_involvement = models.IntegerField('Number of patrons utilizing community programs.',default=0)
-    # recreation_center_patrons = models.IntegerField('Number of patrons utilizing the recreation center.',default=0)
-    # golf_course_attendees = models.IntegerField('Number of attendees at the golf course.',default=0)
-    # ice_arena_participants = models.IntegerField('Participants at the ice arena.',default=0)
+class CivicHealth(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=RECREATION_CATEGORIES, max_length=100)
+    category = models.ForeignKey(SubCategories)
     value = models.DecimalField(decimal_places=2, max_digits=11)
     month = models.CharField(choices=MONTHS, max_length=20)
     year = models.IntegerField(choices=YEARS)
 
     def __str__(self):
-        return "Database table for Recreation and Cultural."
+        return "{0} - {1}, {2}".format(self.category, self.month, self.year)
 
 
-class Economic(models.Model):
-    # active_business_licenses = models.IntegerField('Active number of business licenses.',default=0)
-    # building_permits_given = models.IntegerField('Number of building permits distributed.',default=0)
-    # Need to decide how this is going to be stored. 0.33 or 33 for 33%.
-    # unemployment_rate = models.DecimalField('Unemployment rate as a percentage.',
-    #                                     default=0.0,max_digits=5,decimal_places=5)
-    # houses_sold = models.IntegerField('Number of houses sold.', default=0)
+class EconomicHealth(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=ECONOMIC_CATEGORIES, max_length=100)
+    category = models.ForeignKey(SubCategories)
     value = models.DecimalField(decimal_places=2, max_digits=11)
     month = models.CharField(choices=MONTHS, max_length=20)
     year = models.IntegerField(choices=YEARS)
 
     def __str__(self):
-        return "Database table for Economic Health."
+        return "{0} - {1}, {2}".format(self.category, self.month, self.year)
 
 
-class Environment(models.Model):
-    # waste_collected = models.IntegerField('Number of truckloads of waste collected.',default=0) # Number of Truckloads
-    # Number of Truckloads
-    # recyclables_collected = models.IntegerField('Number of truckloads of recyclables collected.',default=0)
-    # % of capacity. Need to decide how this is going to be stored. 0.33 or 33 for 33%.
-    # level_reserve_water_tanks = models.DecimalField('Level of reserve water tanks as a percentage of capacity.'
-    #                                                                   ,default=0.0,max_digits=5,decimal_places=5)
-    # Could maybe change to an integer. max_digits=14 and decimal_places=2 allows up to 999,999,999,999.99 gallons
-    #  of water to be stored. (Unless I'm mistaken...)
-    # gallons_water_treated = models.DecimalField('Gallons of water treated',default=0.0,max_digits=14,decimal_places=2)
-    # home_inspections = models.IntegerField('Number of home inspections.',default=0)
-    # business_inspections = models.IntegerField('Number of business inspections.',default=0)
+class GovernmentPerformance(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=ENVIRONMENT_CATEGORIES, max_length=100)
+    category = models.ForeignKey(SubCategories)
     value = models.DecimalField(decimal_places=2, max_digits=11)
     month = models.CharField(choices=MONTHS, max_length=20)
     year = models.IntegerField(choices=YEARS)
 
     def __str__(self):
-        return "Database table for Environmental Health."
+        return "{0} - {1}, {2}".format(self.category, self.month, self.year)
 
 
 class Safety(models.Model):
-    # nonviolent_crimes_reported = models.IntegerField('Number of nonviolent crimes reported.',default=0)
-    # violent_crimes_reported = models.IntegerField('Number of violent crimes reported.',default=0)
-    # Might need to change to police_response_time_high_priority
-    # police_response_time = models.DecimalField('Police response time',default=0.0,max_digits=10,decimal_places=2)
-    # fires_responded = models.IntegerField('Number of fires responded to.',default=0)
-    # fire_response_time = models.DecimalField('Fire department response time,default=0.0,max_digits=9,decimal_places=2)
     id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=SAFETY_CATEGORIES, max_length=100)
-    value = models.DecimalField(decimal_places=2, max_digits=11)
-    month = models.CharField(choices=MONTHS, max_length=20)
-    year = models.IntegerField(choices=YEARS)
-
-    def __str__(self):
-        return "Database table for Public Safety."
-
-
-class Transportation(models.Model):
-    # transportation_injuries = models.IntegerField('Transportation injuries.', default=0)
-    # ontime_percent = models.DecimalField('Percentage of services on time.', default=0.0,max_digits=5,decimal_places=5)
-    # auto_accidents = models.IntegerField('Number of auto accidents.', default=0)
-    # In thousands. Multiply by 1000 if necessary. Storing 19.1 would equal 19,100.
-    # public_ridership = models.DecimalField('Ridership',default=0.0,max_digits=7,decimal_places=3)
-    # potholes_filed_YTD = models.IntegerField('Potholes filled YTD.', default=0)
-    id = models.IntegerField(primary_key=True, auto_created=True, editable=False)
-    category = models.CharField(choices=TRANSPORTATION_CATEGORIES, max_length=100)
+    category = models.ForeignKey(SubCategories)
     value = models.DecimalField(decimal_places=2, max_digits=11)
     month = models.CharField(choices=MONTHS, max_length=20)
     year = models.IntegerField(choices=YEARS)
